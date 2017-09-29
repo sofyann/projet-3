ec9e00e85ad3350c6f023b3a79da0c49c6418d2e
sofyann <sofyann.fahim@gmail.com>
2017-09-29 23:32:37 +0200

    Mise en place des notifications à une date précise

--- app/src/main/java/fr/fahim/sofyann/multilinguaprojet3/DatesDeCours.java
+++ app/src/main/java/fr/fahim/sofyann/multilinguaprojet3/DatesDeCours.java
@@ -1,48 +1,34 @@
 package fr.fahim.sofyann.multilinguaprojet3;
 
-import android.app.usage.UsageEvents;
+
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.LinearLayout;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
-import java.util.Locale;
+
 
 public class DatesDeCours extends AppCompatActivity {
-
-    Button listDatesBtn;
-    Button calendrierBtn;
-    LinearLayout linearLayout;
-    CalendarView calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dates_de_cours);
-        listDatesBtn = (Button)findViewById(R.id.listProchainesDates);
-        calendrierBtn = (Button)findViewById(R.id.calendrier);
-        linearLayout = (LinearLayout)findViewById(R.id.layoutListView);
-        calendar = (CalendarView) findViewById(R.id.calendarView);
-        calendrierBtn.setEnabled(false);
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Rdv");
         query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        query.whereEqualTo("rdvPasser", false);
         query.orderByAscending("createdAt");
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
@@ -58,32 +44,41 @@ public class DatesDeCours extends AppCompatActivity {
                     }else {
                         Toast.makeText(DatesDeCours.this, "Aucun cours programmer", Toast.LENGTH_SHORT).show();
                     }
-
                 }
             }
         });
-
-}
+    }
     private void displayDates(ArrayList<ItemDate> itemDates){
         ListView listView = (ListView)findViewById(R.id.listViewDates);
         ItemAdapter adapter = new ItemAdapter(this, R.layout.itemdate_layout, itemDates);
         listView.setAdapter(adapter);
-
-
+        generateNotifications(itemDates);
     }
 
-    public void listProchainesDates(View view){
-        listDatesBtn.setEnabled(false);
-        calendrierBtn.setEnabled(true);
-        calendar.animate().translationXBy(2500f).setDuration(1000);
-        linearLayout.animate().translationXBy(2500f).setDuration(1000);
+    private void generateNotifications(ArrayList<ItemDate> itemDates){
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        for(int i = 0; i < itemDates.size(); i++){
+            String dateString = itemDates.get(i).getMonth()+"/"+itemDates.get(i).getDay()+"/"+itemDates.get(i).getYear()+" "+itemDates.get(i).getHour()+":"+itemDates.get(i).getMinutes()+":00";
+            try {
+                Date date = sdf.parse(dateString);
+                Date date1 = Calendar.getInstance().getTime();
+                Log.i("date1", String.valueOf(date1));
+                
+            } catch (java.text.ParseException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    public void calendrier(View view){
 
-        listDatesBtn.setEnabled(true);
-        calendrierBtn.setEnabled(false);
-        calendar.animate().translationXBy(-2500f).setDuration(1000);
-        linearLayout.animate().translationXBy(-2500f).setDuration(1000);
+    private Notification getNotification(String content) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 1, intent, 0);
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Multilingua");
+        builder.setContentText(content);
+        builder.setAutoCancel(true);
+        builder.setContentIntent(pendingIntent);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        return builder.build();
     }
-
 }
---
+++ app/src/main/java/fr/fahim/sofyann/multilinguaprojet3/NotificationPublisher.java
@@ -0,0 +1,24 @@
+package fr.fahim.sofyann.multilinguaprojet3;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Sofyann on 29/09/2017.
+ */
+
+public class NotificationPublisher extends BroadcastReceiver {
+    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION = "notification";
+
+    public void onReceive(Context context, Intent intent) {
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        notificationManager.notify(id, notification);
+    }
+}
